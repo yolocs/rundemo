39bc2270c37cf6ca56dd35d7279974e3449a844e
Chen Shou <cshou@google.com>
2021-07-10 02:57:10 +0000

    Add a http server that returns animal figures

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/yolocs/rundemo
+
+go 1.16
+
+require (
+	github.com/Code-Hex/Neo-cowsay v1.0.1 // indirect
+	github.com/golang/glog v0.0.0-20210429001901-424d2337a529 // indirect
+	github.com/gomodule/redigo v1.8.5 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.1.0 // indirect
+	github.com/jackc/pgx/v4 v4.11.0 // indirect
+	github.com/jessevdk/go-flags v1.5.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,273 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	cowsay "github.com/Code-Hex/Neo-cowsay"
+	"github.com/golang/glog"
+	"github.com/gomodule/redigo/redis"
+	"github.com/gorilla/mux"
+	"github.com/kelseyhightower/envconfig"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const (
+	CacheHitHeader = "x-cow-cache-hit"
+)
+
+type SQLConn struct {
+	Database string `envconfig:"DB_NAME"`
+	User     string `envconfig:"DB_USER"`
+	Password string `envconfig:"DB_PASS"`
+	Socket   string `envconfig:"DB_SOCKET"`
+}
+
+type RedisConn struct {
+	Host string `envconfig:"REDIS_HOST"`
+	Port int    `envconfig:"REDIS_PORT"`
+}
+
+func main() {
+	var sqlconn SQLConn
+	if err := envconfig.Process("", &sqlconn); err != nil {
+		glog.Exit(err)
+	}
+	var redisconn RedisConn
+	if err := envconfig.Process("", &redisconn); err != nil {
+		glog.Exit(err)
+	}
+
+	db, err := initDB(sqlconn)
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	redispool, err := initRedis(redisconn)
+	if err != nil {
+		glog.Exit(err)
+	}
+
+	wcs := &whatcowsay{
+		db:        db,
+		redispool: redispool,
+	}
+	wcs.setupRoutes()
+
+	glog.Info("Listening on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		glog.Exit(err)
+	}
+}
+
+type whatcowsay struct {
+	db        *sql.DB
+	redispool *redis.Pool
+	r         *mux.Router
+}
+
+func (wcs *whatcowsay) setupRoutes() {
+	wcs.r = mux.NewRouter()
+	cowPath := wcs.r.Path("/cows/{cow}").Subrouter()
+	cowPath.Methods(http.MethodPost).HandlerFunc(wcs.handleCowUpsert)
+	cowPath.Methods(http.MethodGet).HandlerFunc(wcs.handleCowSay)
+	wcs.r.HandleFunc("/admin", wcs.handleAdmin)
+	http.Handle("/", wcs.r)
+}
+
+func (wcs *whatcowsay) handleCowUpsert(w http.ResponseWriter, req *http.Request) {
+	cow := mux.Vars(req)["cow"]
+
+	if wcs.redispool == nil && wcs.db == nil {
+		http.Error(w, "No persistent store available to store what the cow is going to say", http.StatusInternalServerError)
+		return
+	}
+
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, "Failed to read HTTP request body: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	say, err := saySomething(string(b))
+	if err != nil {
+		http.Error(w, "Failed to generate figure: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if wcs.db == nil {
+		glog.Warningf("DB is not used; will save the figure %q to cache", cow)
+		if err := wcs.setRedisVal(cow, say); err != nil {
+			http.Error(w, "Failed to save to cache: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		return
+	}
+
+	if err := wcs.passThroughSaveVal(cow, say); err != nil {
+		http.Error(w, "Failed to save to db: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (wcs *whatcowsay) handleCowSay(w http.ResponseWriter, req *http.Request) {
+	cow := mux.Vars(req)["cow"]
+
+	if wcs.redispool == nil && wcs.db == nil {
+		t, err := saySomething(fmt.Sprintf("Hi, I'm %s. Temporary. Don't count on me~", cow))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(t))
+		return
+	}
+
+	if wcs.redispool != nil {
+		t, err := wcs.getRedisVal(cow)
+		// If we can get the value from the cache.
+		if err == nil && t != "" {
+			w.Header().Set(CacheHitHeader, "true")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(t))
+			return
+		}
+		if err != nil {
+			glog.Warningf("Redis error: %v", err)
+		}
+	}
+
+	t, err := wcs.passThroughGetVal(cow)
+	if err == sql.ErrNoRows {
+		http.Error(w, "No figure found!", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, "DB error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(t))
+}
+
+func (wcs *whatcowsay) getRedisVal(key string) (string, error) {
+	if wcs.redispool == nil {
+		return "", errors.New("Redis is not used")
+	}
+	c := wcs.redispool.Get()
+	defer c.Close()
+	c.Send("GET", key)
+	c.Flush()
+	return redis.String(c.Receive())
+}
+
+func (wcs *whatcowsay) setRedisVal(key, val string) error {
+	if wcs.redispool == nil {
+		return errors.New("Redis is not used")
+	}
+	c := wcs.redispool.Get()
+	defer c.Close()
+	c.Send("SET", key, val)
+	c.Send("EXPIRE", key, "20")
+	c.Flush()
+	if _, err := c.Receive(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (wcs *whatcowsay) passThroughSaveVal(key, val string) error {
+	q := `INSERT INTO cows (name, message)
+VALUES ($1, $2)
+ON CONFLICT (name)
+DO UPDATE SET message = $2;`
+
+	if _, err := wcs.db.Exec(q, key, val); err != nil {
+		return err
+	}
+	if err := wcs.setRedisVal(key, val); err != nil {
+		glog.Warningf("Failed to pass-through save figure %q in cache", key)
+	}
+	return nil
+}
+
+func (wcs *whatcowsay) passThroughGetVal(key string) (string, error) {
+	var text string
+	err := wcs.db.QueryRow("SELECT message FROM cows WHERE name = $1", key).Scan(&text)
+	if err != nil {
+		return "", err
+	}
+	if err := wcs.setRedisVal(key, text); err != nil {
+		glog.Warningf("Failed to pass-through save figure %q in cache", key)
+	}
+	return text, nil
+}
+
+func (wcs *whatcowsay) handleAdmin(w http.ResponseWriter, req *http.Request) {
+	http.Error(w, "not implemented", http.StatusNotAcceptable)
+}
+
+func saySomething(text string) (string, error) {
+	return cowsay.Say(
+		cowsay.Phrase(text),
+		cowsay.BallonWidth(40),
+		cowsay.Random(),
+	)
+}
+
+func initRedis(redisconn RedisConn) (*redis.Pool, error) {
+	if redisconn.Host == "" {
+		return nil, nil
+	}
+
+	redisAddr := fmt.Sprintf("%s:%d", redisconn.Host, redisconn.Port)
+	const maxConnections = 20
+	return &redis.Pool{
+		MaxIdle: maxConnections,
+		Dial:    func() (redis.Conn, error) { return redis.Dial("tcp", redisAddr) },
+	}, nil
+}
+
+func initDB(sqlconn SQLConn) (*sql.DB, error) {
+	if sqlconn.Database == "" {
+		return nil, nil
+	}
+
+	db, err := initSocketConnectionPool(sqlconn)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = db.Exec(`CREATE TABLE IF NOT EXISTS cows ( name VARCHAR(255) NOT NULL, message TEXT NOT NULL, PRIMARY KEY (name) );`); err != nil {
+		return nil, fmt.Errorf("DB.Exec: unable to create table: %w", err)
+	}
+
+	return db, nil
+}
+
+func initSocketConnectionPool(sqlconn SQLConn) (*sql.DB, error) {
+	var dbURI string
+	dbURI = fmt.Sprintf("user=%s password=%s database=%s host=%s", sqlconn.User, sqlconn.Password, sqlconn.Database, sqlconn.Socket)
+
+	// dbPool is the pool of database connections.
+	dbPool, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %v", err)
+	}
+
+	configureConnectionPool(dbPool)
+	return dbPool, nil
+}
+
+func configureConnectionPool(dbPool *sql.DB) {
+	dbPool.SetMaxIdleConns(5)
+	dbPool.SetMaxOpenConns(7)
+	dbPool.SetConnMaxLifetime(1800)
+}
